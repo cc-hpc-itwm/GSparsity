263a14f519e933ed22c4ba114207d3801b89c2ed
Yang Yang <yang.yang@itwm.fraunhofer.de>
2022-04-25 14:17:27 +0200

    remove logs from operation/filter pruning

--- filter_pruning/logs_fpgm/retrain_fpgm_pretrained_all_conv_bn_pruned_lr_0.1_momentum_0.9_wd_0.0001_20211105-130436/scripts/list_of_models.py
+++
@@ -1,7 +0,0 @@
-pretrained_div_mu_0_05 = 'all_conv_lr_0.001_momentum_0.9_wd_0.05_normalization_div_pretrained_True_20211031-160749'
-
-mul_mu_0_00005_epoch81 = 'regroup_normalization_resnet50_lr_0.001_momentum_0.9_wd_5e-05_20211029-150925'
-mul_mu_0_00006 = 'regroup_normalization_resnet50_lr_0.001_momentum_0.9_wd_6e-05_normalization_mul_20211031-083445'
-
-fpgm_scratch = '_fpgm_best.resnet50.2018-07-22-6975'
-fpgm_pretrained = '_fgpm_pretrained_best.resnet50.2018-07-16-4310'
